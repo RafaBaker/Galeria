2a54d9ffa835c81b0832646f5d957e350d614a12
Rafael Rodrigues <rafael1309mt@gmail.com>
2022-08-26 14:54:59 -0300

    Botão de voltar adicionado

--- app/src/main/java/rodrigues/rafael/galeria/MainActivity.java
+++ app/src/main/java/rodrigues/rafael/galeria/MainActivity.java
@@ -1,6 +1,7 @@
 package rodrigues.rafael.galeria;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -10,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.tbMain);
+        //Seta a toolbar como padrão
+        setSupportActionBar(toolbar);
+
+
     }
 }
--- app/src/main/java/rodrigues/rafael/galeria/PhotoActivity.java
+++ app/src/main/java/rodrigues/rafael/galeria/PhotoActivity.java
@@ -1,6 +1,8 @@
 package rodrigues.rafael.galeria;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -10,5 +12,14 @@ public class PhotoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo);
+
+        Toolbar toolbar = findViewById(R.id.tbPhoto);
+        //Seta a toolbar como padrão
+        setSupportActionBar(toolbar);
+
+        //Pega a ActionBar padrão da activity (setada em cima)
+        ActionBar actionBar = getSupportActionBar();
+        //Habilita o botão de voltar
+        actionBar.setDisplayHomeAsUpEnabled(true);
     }
 }
